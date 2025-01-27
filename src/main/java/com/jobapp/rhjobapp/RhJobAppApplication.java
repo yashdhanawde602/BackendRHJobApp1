1cd2c69c18b0acd9cd9acf5e02c77b1a1596bcfc
yashdhanawde602 <yash.dhanawde@ripplehire.com>
2025-01-27 21:13:57 +0530

    First Backend commit

--- src/main/java/com/jobapp/rhjobapp/RhJobAppApplication.java
+++ src/main/java/com/jobapp/rhjobapp/RhJobAppApplication.java
@@ -2,9 +2,10 @@ package com.jobapp.rhjobapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RhJobAppApplication {
+public class RhJobAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RhJobAppApplication.class, args);
